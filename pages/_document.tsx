1834715b40d8f1660ce95c67b82cafba3633bce2
Baha Striker <bahastriker@outlook.com>
2024-11-19 19:00:03 +0100

    fix google tag script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,13 +25,16 @@ export default function Document() {
         <Main />
         <NextScript />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PCZTQXXRM6"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-PCZTQXXRM6');
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PCZTQXXRM6');
+            `,
+          }}
+        ></script>
       </body>
     </Html>
   );
